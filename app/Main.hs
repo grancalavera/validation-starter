08c5d3ad68e85b8aed59de22b1efd1e2cdb429dd
Leon Coto <leoncoto@gmail.com>
2019-03-31 22:31:23 +0100

    improve errors and improve value extraction

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,20 @@ import           Lib
 
 main :: IO ()
 main = do
-  print $ mkDivisor 0
-  print $ mkDivisorPair 0 0
-  print $ mkDivisorPair 0 9
-  print $ mkDivisor 9
-  print $ mkEven 9
-  print $ mkEven 2
-  print $ mkEvenPair 9 5
-  print $ mkEvenPair 9 2
-  print $ mkEvenPair 4 2
+
+  -- collections
+  print $ traverse ((value <$>) . mkEven) [1 .. 5]
+  print $ traverse ((value <$>) . mkEven) [2, 4 .. 10]
+
+  -- singles
+  print $ value <$> mkDivisor 0
+  print $ value <$> mkDivisor 9
+  print $ value <$> mkEven 9
+  print $ value <$> mkEven 2
+
+  -- pairs
+  print $ pairValue <$> mkEvenPair 9 2
+  print $ pairValue <$> mkEvenPair 9 5
+  print $ pairValue <$> mkEvenPair 4 2
+  print $ pairValue <$> mkDivisorPair 0 9
+  print $ pairValue <$> mkDivisorPair 0 0
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,42 +3,43 @@ module Lib
     , mkDivisorPair
     , mkEven
     , mkEvenPair
+    , value
+    , pairValue
     )
 where
 
 import           Data.Validation
-import           Data.List.NonEmpty                       ( NonEmpty((:|)) )
-import qualified Data.List.NonEmpty            as NonEmpty
+import           Data.Bifunctor
 
-newtype Err = Err { unErr ::  NonEmpty String }
-
-instance Semigroup Err where
-    left <> right = Err (unErr left <> unErr right)
+data Err = DivisorIsZero | IsNotEven Int
 
 instance Show Err where
-    show (Err (e :| [])) = show e
-    show (Err es       ) = show (NonEmpty.toList es)
+    show DivisorIsZero = "Error: zero can't be a divisor"
+    show (IsNotEven n) = "Error: " <> show n <> " is not an even integer"
 
-newtype Divisor = Divisor { unDivisor:: Int } deriving Show
+class ValidInt a where
+    value :: a -> Int
+    pairValue :: (a, a) -> (Int, Int)
+    pairValue = bimap value value
 
-newtype Even = Even { unEven::Int } deriving Show
+newtype Divisor = Divisor { unDivisor :: Int } deriving Show
 
-mkDivisor :: Int -> Validation Err Divisor
-mkDivisor n = Divisor <$> validate (mkErr "Divisor can't be zero") isNotZero n
+instance ValidInt Divisor where
+    value = unDivisor
 
-mkDivisorPair :: Int -> Int -> Validation Err (Divisor, Divisor)
+newtype Even = Even { unEven :: Int } deriving Show
+
+instance ValidInt Even where
+    value = unEven
+
+mkDivisor :: Int -> Validation [Err] Divisor
+mkDivisor n = Divisor <$> validate [DivisorIsZero] (/= 0) n
+
+mkDivisorPair :: Int -> Int -> Validation [Err] (Divisor, Divisor)
 mkDivisorPair x y = (,) <$> mkDivisor x <*> mkDivisor y
 
-isNotZero :: Int -> Bool
-isNotZero 0 = False
-isNotZero _ = True
+mkEven :: Int -> Validation [Err] Even
+mkEven n = Even <$> validate [IsNotEven n] even n
 
-
-mkErr :: String -> Err
-mkErr = Err . (:| [])
-
-mkEven :: Int -> Validation Err Even
-mkEven n = Even <$> validate (mkErr (show n <> " is not even")) even n
-
-mkEvenPair :: Int -> Int -> Validation Err (Even, Even)
+mkEvenPair :: Int -> Int -> Validation [Err] (Even, Even)
 mkEvenPair x y = (,) <$> mkEven x <*> mkEven y
